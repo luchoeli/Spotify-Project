72ef41221e6acfdacbb532d0dcfbc54460693424
Luciano Elissondo <luciano.elissondo@globant.com>
2019-01-08 11:09:51 -0300

    albumCard functionalComponent

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -2,21 +2,20 @@ import React from 'react'
 import '../stylecheet/FavoriteStyle.css'
 import { Link } from 'react-router-dom'
 
-class AlbumCard extends React.Component {
-    render() {
-        return (
-            <Link style={{ textDecoration: 'none' , color:'black'}} to={`/album/${this.props.id}`}>
+function AlbumCard(props) {
+    return (
+        <Link style={{ textDecoration: 'none' , color:'black'}} to={`/album/${props.id}`}>
 
-                <div className="MusicCard"> 
-                        <img src={this.props.albumImg} alt={'imagen of artist'} />
-                        <div className="SongInfo">
-                            <p><strong>{this.props.albumName}</strong></p>
-                            <p>{this.props.albumReleaseDate}</p>
-                        </div>                   
-                </div>
-                
-            </Link>
-        );
-    }
-}
+            <div className="MusicCard"> 
+                    <img src={props.albumImg} alt={'imagen of artist'} />
+                    <div className="SongInfo">
+                        <p><strong>{props.albumName}</strong></p>
+                        <p>{props.albumReleaseDate}</p>
+                    </div>                   
+            </div>
+            
+        </Link>
+    );
+  }
+
 export default AlbumCard;
